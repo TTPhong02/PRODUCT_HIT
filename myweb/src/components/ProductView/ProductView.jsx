268e125e966110572f176c53973e4c9ac2f10f17
ThoNguyen <hatigr.th@gmail.com>
2022-06-16 17:57:00 +0700

    [NguyenTho] coding view product....

--- myweb/src/components/ProductView/ProductView.jsx
+++ myweb/src/components/ProductView/ProductView.jsx
@@ -3,12 +3,22 @@ import "./productView.scss";
 import Brands from "../Brands/Brands";
 import InfoProduct from "../InfoProduct/InfoProduct";
 import ViewImagesProduct from "../ViewImagesProduct/ViewImagesProduct";
+import ProductReviews from "../ProductReviews/ProductReviews";
+import AdditionalInformation from "../AdditionialInfomation/AdditionalInformation";
 const ProductView = () => {
   return (
-    <div>
+    <div className="contain-page">
       <div className="info-product">
         <ViewImagesProduct />
         <InfoProduct />
+      </div>
+      <div className="more-info">
+        <div className="nav-more">
+          <p className="addition-info active">Thông tin bổ sung</p>
+          <p className="reviews-product">Đánh giá (123)</p>
+        </div>
+        <ProductReviews />
+        <AdditionalInformation />
       </div>
       <Brands />
     </div>
